a04ae3b6da8253475253c8070fc3473aaf5359bf
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2023-02-28 17:07:20 -0600

    Cambio de puerto y scripts en package.json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
 //Vamos a hacer algunas importaciones
 const cors = require("cors");
 //Vamos a importar dotenv
